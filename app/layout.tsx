a7aa470ff2b5614fd0109982e3e321a6f28d4549
jsacap <sanchoalegre@gmail.com>
2024-02-23 15:19:45 +1100

    Added simple markdown editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme appearance="dark" accentColor="iris" radius="large">
+        <Theme appearance="light" accentColor="iris" radius="large">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
