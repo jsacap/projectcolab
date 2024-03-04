b625e7e8499ed9c9ace1cad08f1381d44bbc2983
jsacap <sanchoalegre@gmail.com>
2024-03-05 10:04:52 +1100

    file restructure to remove duplicate skeletons

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -34,7 +34,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       setSubmitting(true);
       if (issue) await axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues/", data);
-      router.push("/issues");
+      router.push("/issues/push");
       router.refresh();
     } catch (error) {
       setSubmitting(false);
